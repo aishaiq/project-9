5ab98c4b31ba9a2637be4c1340bc389b99f9d857
aishaiq <117289120+aishaiq@users.noreply.github.com>
2022-11-07 15:54:38 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+
+function setup() { createCanvas(400,400); 
+  background(51); 
+ 
+}
+
+
+function draw() {
+// var box = createSprite(200,200,30,30); 
+if (keyIsDown(RIGHT_ARROW)) { 
+background("red");
+
+ }
+
+// drawSprites() ;
+}
